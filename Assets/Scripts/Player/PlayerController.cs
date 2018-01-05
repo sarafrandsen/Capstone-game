a64da231d05b46ca62a2ba2604fbd617e0e3f15e
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-04 22:58:09 -0800

    character animates and transitions from idle to walking animations in 4 cardinal directions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,13 +6,19 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 5;
 
+    private Animator anim;
+    private bool isMoving;
+    private Vector2 lastMove;
+
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
     void Update () {
+
+        isMoving = false;
 
         // get the input from the keyboard
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -26,11 +32,22 @@ public class PlayerController : MonoBehaviour {
         if (Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical))
         {
             moveVertical = 0;
-        } else {
+            isMoving = true;
+            lastMove = new Vector2(horizontal, 0f);
+        } else if (Mathf.Abs(moveHorizontal) < Mathf.Abs(moveVertical)) {
             moveHorizontal = 0;
-        } 
+            isMoving = true;
+            lastMove = new Vector2(0f, vertical);
+        } else {
+            isMoving = false;
+        }
 
         // this tells it the coordinates to move to
         transform.Translate (new Vector2(moveHorizontal, moveVertical));
+        anim.SetBool("IsMoving", isMoving);
+        anim.SetFloat("MoveX", horizontal);
+        anim.SetFloat("MoveY", vertical);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
     }﻿
 }
