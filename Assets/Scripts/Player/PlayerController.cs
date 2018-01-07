aed4cadb649611da0186c936ae0ae7fd0115462e
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-06 16:02:34 -0800

    added velocity to player movement to prevent jittery collision

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour {
     private Animator anim;
     private bool isMoving;
     private Vector2 lastMove;
+    private Rigidbody2D myRigBod;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        myRigBod = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -21,29 +23,36 @@ public class PlayerController : MonoBehaviour {
         isMoving = false;
 
         // get the input from the keyboard
+        // absolute value keeps from going diagonal
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        // determine the speed in seconds per frame
-        float moveHorizontal = horizontal * moveSpeed * Time.deltaTime;
-        float moveVertical = vertical * moveSpeed * Time.deltaTime;
-
-        // absolute value keeps player from moving diagonally
-        if (Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical))
+        if (horizontal > 0.5f || horizontal < -0.5f)
         {
-            moveVertical = 0;
+            myRigBod.velocity = new Vector2(horizontal * moveSpeed, myRigBod.velocity.y);
             isMoving = true;
-            lastMove = new Vector2(horizontal, 0f);
-        } else if (Mathf.Abs(moveHorizontal) < Mathf.Abs(moveVertical)) {
-            moveHorizontal = 0;
-            isMoving = true;
-            lastMove = new Vector2(0f, vertical);
-        } else {
-            isMoving = false;
+            lastMove = new Vector2(horizontal, 0);
         }
 
+        if (vertical > 0.5f || vertical < -0.5f)
+        {
+            myRigBod.velocity = new Vector2(myRigBod.velocity.x, vertical * moveSpeed);
+            isMoving = true;
+            lastMove = new Vector2(0, vertical);
+        }
+
+        if (horizontal < 0.5f && horizontal > -0.5f)
+        {
+            myRigBod.velocity = new Vector2(0, myRigBod.velocity.y);
+        }
+
+        if (vertical < 0.5f && vertical > -0.5f)
+        {
+            myRigBod.velocity = new Vector2(myRigBod.velocity.x, 0);
+        }
+
+
         // this tells it the coordinates to move to
-        transform.Translate (new Vector2(moveHorizontal, moveVertical));
         anim.SetBool("IsMoving", isMoving);
         anim.SetFloat("MoveX", horizontal);
         anim.SetFloat("MoveY", vertical);
