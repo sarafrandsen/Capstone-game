5d6c831dbc4a9648558bb456d3f775d6aa8bfdc8
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-09 22:07:06 -0800

    got rid of animating dialogue box, fixing bug on key press

--- Assets/Scripts/NPCs/DialogueManager.cs
+++ Assets/Scripts/NPCs/DialogueManager.cs
@@ -7,12 +7,14 @@ public class DialogueManager : MonoBehaviour {
 
     public Text nameText;
     public Text dialogueText;
-    public Animator animator; // reference animator controller in order to control the dialog box animation
+    //public Animator animator; // reference animator controller in order to control the dialog box animation 
     public TextAsset textFile; // TextAsset: literally text file assets
     public string[] textLines; // each line of text asset is assigned to an element
     public int currentLine;
     public int endAtLine;
     public PlayerController player;
+    public bool dialogueIsActive;
+    public GameObject dialogueBox;
 
 
     private Queue<string> sentences; // FIFO collection
@@ -26,22 +28,44 @@ public class DialogueManager : MonoBehaviour {
             textLines = (textFile.text.Split('\n')); // split at line break
         }
 
-        player = FindObjectOfType<PlayerController>();
+        player = FindObjectOfType<PlayerController>(); // using on the PlayerController
 
         if (endAtLine == 0) // if endAtLine is not defined
         {
             endAtLine = textLines.Length - 1; // default to every line
         }
+
+        if (dialogueIsActive)
+        {
+            EnableTextBox();
+        } else {
+            DisableTextBox();
+        }
     }
 
     void Update()
     {
+        if (!dialogueIsActive)
+        {
+            return;
+        }
+
         dialogueText.text = textLines[currentLine];
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            currentLine += 1;
+        }
+
+        if (currentLine > endAtLine)
+        {
+            DisableTextBox();
+        }
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-        animator.SetBool("IsOpen", true); // open dialogue box when conversation starts
+        //animator.SetBool("IsOpen", true); // open dialogue box when conversation starts 
 
         nameText.text = dialogue.name;
         sentences.Clear(); // clear any previous sentences
@@ -71,8 +95,18 @@ public class DialogueManager : MonoBehaviour {
 
     void EndDialogue()
     {
-        animator.SetBool("IsOpen", false); // close box at the end of the dialogue
+        //animator.SetBool("IsOpen", false); // close box at the end of the dialogue
         Debug.Log("End of conversation");
+    }
+
+    public void EnableTextBox()
+    {
+        dialogueBox.SetActive(true);
+    }
+
+    public void DisableTextBox()
+    {
+        dialogueBox.SetActive(false);
     }
 
     IEnumerator TypeSentence(string sentence) // instead of updating text directly, using coroutine
