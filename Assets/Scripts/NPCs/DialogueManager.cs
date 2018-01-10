e1a2333fea9cafbe672a6525dd80564645b98023
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-09 19:55:57 -0800

    create simple slide animation for dialogue boxes appearing on the screen

--- Assets/Scripts/NPCs/DialogueManager.cs
+++ Assets/Scripts/NPCs/DialogueManager.cs
@@ -5,33 +5,33 @@ using UnityEngine.UI; // necessary for UI objects like Text
 
 public class DialogueManager : MonoBehaviour {
 
- //   public GameObject dialogBox;
- //   public Text dialogText;
- //   public bool dialogActive;
- //   public float showTime = 0.5f;
+    //   public GameObject dialogBox;
+    //   public Text dialogText;
+    //   public bool dialogActive;
+    //   public float showTime = 0.5f;
 
- //   private float showCounter;
+    //   private float showCounter;
 
-	//// Use this for initialization
-	//void Start () {
-        
-	//}
-	
-	//// Update is called once per frame
-	//void Update () {
- //       // if the dialogue box is up and the user presses 'space'
- //       if (dialogActive)
- //       {
- //           showCounter -= Time.deltaTime;
+    //// Use this for initialization
+    //void Start () {
 
- //           if (Input.GetKeyDown(KeyCode.Space) && showCounter < 0)
- //           {
- //               Debug.Log("Hide");
- //               dialogBox.SetActive(false); // close the dialog box obj on screen
- //               dialogActive = false;
- //           }
- //       }
-	//}
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+    //       // if the dialogue box is up and the user presses 'space'
+    //       if (dialogActive)
+    //       {
+    //           showCounter -= Time.deltaTime;
+
+    //           if (Input.GetKeyDown(KeyCode.Space) && showCounter < 0)
+    //           {
+    //               Debug.Log("Hide");
+    //               dialogBox.SetActive(false); // close the dialog box obj on screen
+    //               dialogActive = false;
+    //           }
+    //       }
+    //}
 
     //public void ShowDialogBox(string dialog)
     //{
@@ -47,6 +47,10 @@ public class DialogueManager : MonoBehaviour {
     //}
 
 
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator; // reference animator controller in order to control the dialog box animation
+
     private Queue<string> sentences; // FIFO collection
 
     void Start()
@@ -56,7 +60,9 @@ public class DialogueManager : MonoBehaviour {
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Start conversation with " + dialogue.name);
+        animator.SetBool("IsOpen", true); // open dialogue box when conversation starts
+
+        nameText.text = dialogue.name;
         sentences.Clear(); // clear any previous sentences
 
         foreach (string sentence in dialogue.sentences)
@@ -76,11 +82,12 @@ public class DialogueManager : MonoBehaviour {
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        dialogueText.text = sentence;
     }
 
     void EndDialogue()
     {
+        animator.SetBool("IsOpen", false); // close box at the end of the dialogue
         Debug.Log("End of conversation");
     }
 }
