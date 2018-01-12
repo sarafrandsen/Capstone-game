3f250c8d77f15d91ab4cff75c00446d5045f9821
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-11 17:13:22 -0800

    camera stays in bounds

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    
+    public BoxCollider2D boundsBox;
     public GameObject followTarget;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
     private Vector3 offset;
     private static bool cameraExists;
 
@@ -19,10 +25,34 @@ public class CameraController : MonoBehaviour {
         } else {
             Destroy(gameObject);
         }
+
+        minBounds = boundsBox.bounds.min;
+        maxBounds = boundsBox.bounds.max;
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     private void LateUpdate()
     {
         transform.position = followTarget.transform.position + offset;
+        // clamp: given a min val and a max val, make sure the current vals do not exceed
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+
+        if (boundsBox == null)
+        {
+            boundsBox = FindObjectOfType<Bounds>().GetComponent<BoxCollider2D>();
+            minBounds = boundsBox.bounds.min;
+            maxBounds = boundsBox.bounds.max;
+        }
+    }
+
+    public void SetBounds(BoxCollider2D newBounds)
+    {
+        boundsBox = newBounds;
+        minBounds = boundsBox.bounds.min;
+        maxBounds = boundsBox.bounds.max;
     }
 }
