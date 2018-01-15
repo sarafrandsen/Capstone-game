74d37e83e9d3b5b9bcb4e3bf0e03a738718d3678
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-15 14:26:45 -0800

    set up all colliders for all four scenes, create prefabs for entrances, set up different trigger areas for entrances, exits, and dialogue bubbles

--- Assets/Scripts/NPCs/NPCController.cs
+++ Assets/Scripts/NPCs/NPCController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCController : MonoBehaviour {
+public class NPCController : MonoBehaviour
+{
+    public bool canInteract;
+    public bool dialogueComplete;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // if canInteract, play bubble interaction
+    // if dialogueComplete, play slime melt anim and get rid of rigidbody,
+    // access entrance back to main room
 }
