aa962b0535673fa45bf81870706184ac2b6dd927
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-15 20:48:14 -0800

    add conditions for fire and slime animations based on conversations

--- Assets/Scripts/FireObjects/FireObject.cs
+++ Assets/Scripts/FireObjects/FireObject.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class FireObject : MonoBehaviour {
     private GameData gameData;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
+        // opening dialogue with instructions
+        // set dialogue as active?
+
+
+        anim = GetComponent<Animator>();
+
         // check in GameData if true or false
         // false means still accessible
         // true means inaccessible
@@ -18,7 +25,8 @@ public class FireObject : MonoBehaviour {
 	}
 
     void ExtinguishFire() {
-        // change animation to empty fire
+        // change animation to empty fire 
+        anim.SetBool("FireExtinguished", true);
         // disable 'load new area' script
         this.GetComponent<LoadNewArea>().enabled = false;
     }
