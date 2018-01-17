203e38f5b60272fd1f1481dc4287116085b67e42
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-17 10:38:15 -0800

    set up final fire obj

--- Assets/Scripts/FireObjects/FinalFireObject.cs
+++ Assets/Scripts/FireObjects/FinalFireObject.cs
@@ -6,22 +6,20 @@ public class FinalFireObject : MonoBehaviour
 {
     private GameData gameData;
     private Animator anim;
-    private LoadNewArea loadNewArea;
+    private BoxCollider2D boxCollider;
 
     // Use this for initialization
     void Start()
     {
-        // opening dialogue with instructions
-        // set dialogue as active?
-
         gameData = FindObjectOfType<GameData>();
         anim = GetComponent<Animator>();
-        loadNewArea = GetComponent<LoadNewArea>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        anim.SetBool("FireExtinguished", true);
 
         // check in GameData if true or false
         // only accessible if all are true
         string fireDoor = this.tag;
-        if (gameData.fireDoors[fireDoor] == true)
+        if (!gameData.fireDoors.ContainsValue(false))
         {
             BuildFire();
         }
@@ -32,6 +30,6 @@ public class FinalFireObject : MonoBehaviour
         // change animation to lit fire 
         anim.SetBool("FireExtinguished", false);
         // enable 'load new area' script
-        loadNewArea.enabled = true;
+        boxCollider.isTrigger = true;
     }
 }
