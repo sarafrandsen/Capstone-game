834342a8dd75f9e41c0eec830ac3c7694f7c7463
Sara Frandsen <sara.frandsen@gmail.com>
2018-01-18 14:34:26 -0800

    changed sprite renderer in canvas to ui image, stil having problems rendering profile images

--- Assets/Scripts/Twitter/TwitterImage.cs
+++ Assets/Scripts/Twitter/TwitterImage.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 // Get the latest webcam shot from outside "Friday's" in Times Square
 public class TwitterImage : MonoBehaviour
 {
-    private SpriteRenderer dialogueImage;
+    [SerializeField]
+    private Image profileImage;
+    [SerializeField]
+    private Sprite imageSprite;
 
     void Start()
     {
-        dialogueImage = GetComponent<SpriteRenderer>();
-        StartCoroutine(LoadSpriteImage());
+        profileImage = GetComponent<Image>();
+        imageSprite = profileImage.GetComponent<Sprite>();
+
+        StartCoroutine(LoadProfileImage());
     }
 
-    IEnumerator LoadSpriteImage()
+    IEnumerator LoadProfileImage()
     {
         string url = "http://pbs.twimg.com/profile_images/834151634628288512/8BOBiWxh_normal.jpg";
         var www = new WWW(url);
@@ -22,6 +28,6 @@ public class TwitterImage : MonoBehaviour
         Texture2D texture = new Texture2D(1, 1);
         www.LoadImageIntoTexture(texture);
         Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
-        dialogueImage.sprite = sprite;
+        imageSprite = sprite;
     }
 }
